b7b1cac3ccf541247b20b415e201719bb357dd3a
Matheus Marques <matheuspbmarques@protonmail.com>
2026-03-01 12:56:04 -0300

    chore: setup env based in nestjs doc

--- src/services/database/database.providers.ts
+++ src/services/database/database.providers.ts
@@ -1,11 +1,16 @@
 import mongoose, { Mongoose } from 'mongoose';
 import { PROVIDE } from '@utils/constants';
-import { ENV } from '@utils/env';
 import { Provider } from '@nestjs/common';
 
 export const databaseProviders: Array<Provider> = [
   {
     provide: PROVIDE.DATABASE,
-    useFactory: (): Promise<Mongoose> => mongoose.connect(ENV.MONGO_URI),
+    useFactory: (): Promise<Mongoose> => {
+      if (!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI environment variable is not defined');
+      }
+
+      return mongoose.connect(process.env.MONGO_URI);
+    },
   },
 ];
--- src/utils/env.ts
+++
@@ -1,7 +0,0 @@
-import z from 'zod';
-
-const envSchema = z.object({
-  MONGO_URI: z.string().nonempty('MONGO_URI environment variable is required'),
-});
-
-export const ENV = envSchema.parse(process.env);
